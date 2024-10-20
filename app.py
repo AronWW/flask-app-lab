f262c6a4debfe38934091e0d1a19fdfbd579582d
Ivan Hrabivchuk <ivgrabivchuk2005@gmail.com>
2024-10-20 13:04:55 +0300

    add new post template

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, abort
 
 app = Flask(__name__)
-app.config.from_pyfile("config.py")
+app.config.from_pyfile('config.py')
 
 
 @app.route('/')
 def main():
-    return render_template("hello.html")
+    return render_template('hello.html')
 
 
-@app.route('/homepage') 
+@app.route('/homepage')
 def home():
-    """View for the Home page of your website."""
+    """View foe the Home page of your website"""
     agent = request.user_agent
 
-    return render_template("home.html", agent=agent)
+    return render_template('home.html', agent=agent)
+
 
 @app.route('/hi/<string:name>')
 def greeting(name):
@@ -22,11 +23,35 @@ def greeting(name):
     age = request.args.get('age', 0, int)
     return render_template('hi.html', name=name, age=age)
 
-@app.route("/admin")
+
+@app.route('/admin')
 def admin():
-    to_url = url_for("greetings", name="administrator", age=45, _external=True)
+    to_url = url_for("greeting", name='administrator', age=30, _external=True)
     print(to_url)
     return redirect(to_url)
+
+
+posts = [
+    {"id": 1, 'title': 'My First Post',
+        'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 2, 'title': 'Another Day',
+        'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 3, 'title': 'Flask and Jinja2',
+        'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+]
+
+
+@app.route('/posts')
+def get_posts():
+    return render_template("posts.html", posts=posts)
+
+
+@app.route('/post/<int:id>')
+def get_post(id):
+    if id > len(posts):
+        abort(404)
+    post = posts[id-1]
+    return render_template('detail-post.html', post=post)
 
 
 if __name__ == '__main__':
